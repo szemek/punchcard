4a1dfc1ebf7346b633fa156f779341cc8102542b
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2013-10-20 18:10:28 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'haml-rails'
+
+group :development, :test do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'awesome_print'
+  gem 'pry-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Punchcard::Application.config.secret_key_base = 'a0e4341a9a7540ca688ea4fe7886aeaf66d2542ba9be8ceddc3399f1b15ffe4c3d14d561a26f253866149bb957a574c2afefbdcf0098c3720dc21095ca7fdebe'
