324da7c38c69011067f55a65734b901e39f2cebd
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2013-10-22 08:33:39 +0200

    svg builder: first draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Punchcard::Application.routes.draw do
   root to: 'dashboard#index'
+
+  get '/punchcard' => 'dashboard#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +42,7 @@ Punchcard::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
